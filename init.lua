881fea232cfcd0932db72c2eaaff177c2bc5c11c
djlee <moovinzoo@gmail.com>
2024-03-30 05:11:22 +0900

    feat: change colorscheme tokyo-night -> adwaita

--- init.lua
+++ init.lua
@@ -641,21 +641,20 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+  { -- colorschemes
+    -- use `:Telescope colorscheme`.
+    'Mofiqul/adwaita.nvim',
+    lazy = false,
+    priority = 1000,
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+    -- configure and set on startup
+    config = function()
+      vim.g.adwaita_darker = true -- for darker version
+      vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+      vim.g.adwaita_transparent = true -- makes the background transparent
+      vim.cmd.colorscheme 'adwaita'
+      -- -- You can configure highlights by doing something like:
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
