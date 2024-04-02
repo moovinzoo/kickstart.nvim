2110276ff4aefc733e6ce4016091808ddf592aad
djlee <moovinzoo@gmail.com>
2024-04-03 08:28:01 +0900

    feat: add plugin/vim-matchup

--- init.lua
+++ init.lua
@@ -748,6 +748,10 @@ require('lazy').setup({
     end,
   },
 
+  { -- Enhance matchpairs(%) functionality
+    'andymass/vim-matchup',
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
