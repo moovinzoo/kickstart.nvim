ae422027d405807f9702d8d05dbcc9be12bd15da
djlee <moovinzoo@gmail.com>
2024-04-03 14:09:30 +0900

    feat: add plugin; neoscroll -smooth scrolling

--- init.lua
+++ init.lua
@@ -772,6 +772,13 @@ require('lazy').setup({
     'andymass/vim-matchup',
   },
 
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup {}
+    end,
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
