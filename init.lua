45c833351987105bd0ab8abc8d0e8c816ea8b351
djlee <moovinzoo@gmail.com>
2024-04-04 16:45:50 +0900

    feat: add toggleterm/keymap; open float terminal in project-git-dir

--- init.lua
+++ init.lua
@@ -771,6 +771,37 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>t~', '<cmd>ToggleTerm<CR>', { desc = 'Toggle below [T]erminal on [~]' })
       -- Toggle below [T]erminal on [.]
       vim.keymap.set('n', '<leader>t.', '<cmd>ToggleTerm dir=%:p:h<CR>', { desc = 'Toggle below [T]erminal on [.]' })
+      -- Toggle float [T]erminal on . and run [L]azygit (ref. https://github.com/akinsho/toggleterm.nvim/blob/main/README.md#Custom-terminal-usage)
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new {
+        cmd = 'lazygit',
+        dir = 'git_dir',
+        direction = 'float',
+        float_opts = {
+          border = 'double',
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+          vim.cmd 'startinsert!'
+          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+          vim.cmd 'startinsert!'
+        end,
+      }
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      -- vim.api.nvim_set_keymap(
+      vim.keymap.set(
+        'n',
+        '<leader>tl',
+        '<cmd>lua _lazygit_toggle()<CR>',
+        { noremap = true, silent = true, desc = 'Toggle float [T]erminal on . and run [L]azygit' }
+      )
     end,
   },
 
