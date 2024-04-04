7f53fe503a3cf6a3480f43fc739ef2a2d64121f6
djlee <moovinzoo@gmail.com>
2024-04-04 17:42:12 +0900

    feat: add gitsigns/keymap; navigate git hunks(changes)

--- init.lua
+++ init.lua
@@ -180,6 +180,27 @@ require('lazy').setup({
           vim.keymap.set(mode, l, r, opts)
         end
 
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            return ']c'
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true, desc = 'Go to next [C]hange' })
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            return '[c'
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true, desc = 'Go to prev [C]hange' })
+
       end,
     },
   },
