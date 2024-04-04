7d07784d4970fd32ed177b0a55d9424469861dde
djlee <moovinzoo@gmail.com>
2024-04-05 06:19:18 +0900

    feat: add assistance keymaps

--- init.lua
+++ init.lua
@@ -79,6 +79,12 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Assistance keymaps
+vim.keymap.set('n', 'vv', 'viw', { noremap = true, silent = true, desc = 'Select word' })
+vim.keymap.set({ 'n', 'v' }, 'H', '^', { noremap = true, silent = true, desc = 'Go to first letter' })
+vim.keymap.set({ 'n', 'v' }, 'L', 'g_', { noremap = true, silent = true, desc = 'Go to last letter' })
+vim.keymap.set('n', 'Q', '<cmd>q<CR>', { noremap = true, silent = true, desc = 'Quit' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
