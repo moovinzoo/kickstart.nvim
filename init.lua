8104680695bd8aa1a098187f8d597fb53dc72044
djlee <moovinzoo@gmail.com>
2024-04-05 12:25:19 +0900

    feat: Uninstall plugin/neoscroll; disable smooth-scroll

--- init.lua
+++ init.lua
@@ -878,13 +878,6 @@ require('lazy').setup({
     'andymass/vim-matchup',
   },
 
-  { -- Enable smooth-scrolling
-    'karb94/neoscroll.nvim',
-    config = function()
-      require('neoscroll').setup {}
-    end,
-  },
-
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
