5bf321cc043de61bc7e36591aa9bb72244826e80
djlee <moovinzoo@gmail.com>
2024-04-05 10:36:47 +0900

    feat: add title

--- init.lua
+++ init.lua
@@ -66,6 +66,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Enable title
+vim.opt.title = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
