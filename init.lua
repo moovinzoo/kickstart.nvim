4f0291f6fd547d26681f180d3cae333c34dd23e5
djlee <moovinzoo@gmail.com>
2024-04-06 12:48:08 +0900

    feat: support auto quit feature in plugin/nvim-tree when it is only last window

--- init.lua
+++ init.lua
@@ -1017,6 +1017,30 @@ require('lazy').setup({
     },
     config = function()
       require('nvim-tree').setup {}
+
+      -- Auto close solution based on QuitPre that checks if it's the last window with aware of floating-windows
+      vim.api.nvim_create_autocmd('QuitPre', {
+        callback = function()
+          local tree_wins = {}
+          local floating_wins = {}
+          local wins = vim.api.nvim_list_wins()
+          for _, w in ipairs(wins) do
+            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+            if bufname:match 'NvimTree_' ~= nil then
+              table.insert(tree_wins, w)
+            end
+            if vim.api.nvim_win_get_config(w).relative ~= '' then
+              table.insert(floating_wins, w)
+            end
+          end
+          if 1 == #wins - #floating_wins - #tree_wins then
+            -- Should quit, so we close all invalid windows.
+            for _, w in ipairs(tree_wins) do
+              vim.api.nvim_win_close(w, true)
+            end
+          end
+        end,
+      })
     end,
   },
 
